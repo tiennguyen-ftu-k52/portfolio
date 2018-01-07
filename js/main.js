b9083b1ff1c959a84d30b925e7cf1620d9b4e030
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2018-01-07 18:12:27 +0700

    finish work experience & education section

--- js/main.js
+++ js/main.js
@@ -11,3 +11,16 @@ $(function () {
 $(function () {
   new WOW().init();
 });
+
+/*  =========================================
+*               TIMELINE
+*  =========================================  */
+$(function () {
+  $('.cntl').cntl({
+    revealbefore: 300,
+    anim_class: 'cntl-animate',
+    onreveal: function(e){
+      console.log(e);
+    }
+  });
+});
